40b45a0aeede1e8db959e6f4b3d1e5fdcd2ea34b
BarryDB83 <107511169+BarryDB83@users.noreply.github.com>
2022-06-19 21:28:36 +0200

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,15 +2,15 @@ export const config = {
   env: "prod",
   LOCAL_API_URL: "localhost:3000",
   API_URL: "http://localhost:3000",
-  COLLECTION_NAME: "SpritePunks",
-  COLLECTION_TITLE: "SpritePunks",
-  COLLECTION_DESCRIPTION: 'SpritePunks.',
+  COLLECTION_NAME: "MTVPunks",
+  COLLECTION_TITLE: "MTVPunks",
+  COLLECTION_DESCRIPTION: 'MTVPunks.',
   COLLECTION_IMG_LINK: "https://punkscape.xyz/og.png",
-  STARTING_INDEX: 0,
+  STARTING_INDEX: 1,
   CONTRACT: {
     // if you're not sure, set CONTRACT: {}, will remove opensea link
-    CHAIN: "ethereum", //possible values of ethereum, matic (polygin), klatyn, solana
-    ADDRESS: "0x5537d90a4a2dc9d9b37bab49b490cf67d4c54e91", //note: for solana these values change, so currently not supported
+    CHAIN: "Multivac", //possible values of ethereum, matic (polygin), klatyn, solana
+    ADDRESS: "0xa4b775e2563fa3a7f4d82ada9595383edc4bcb68", //note: for solana these values change, so currently not supported
   },
   WEIGHTS: {},
 };
